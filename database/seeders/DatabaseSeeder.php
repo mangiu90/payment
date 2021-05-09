31f6eaad01e28de47effc4e6312d025fbca42d0e
Manuel <giudicimanuel@gmail.com>
2021-05-09 02:27:53 -0300

    productos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        Storage::deleteDirectory('products');
+
+        Storage::makeDirectory('products');
+
+        Product::factory(50)->create();
     }
 }
