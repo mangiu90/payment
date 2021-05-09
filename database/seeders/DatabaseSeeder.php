31265b584caf0882e3b4402f21094eb5774f95a5
Manuel <giudicimanuel@gmail.com>
2021-05-09 02:36:16 -0300

    articulos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Storage;
@@ -18,9 +19,12 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         Storage::deleteDirectory('products');
+        Storage::deleteDirectory('articles');
 
         Storage::makeDirectory('products');
+        Storage::makeDirectory('articles');
 
         Product::factory(50)->create();
+        Article::factory(50)->create();
     }
 }
