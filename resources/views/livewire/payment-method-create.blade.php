9d7da15a3d29b46c231272eeadaaefe8ccf6af3f
Manuel <giudicimanuel@gmail.com>
2021-05-10 14:45:20 -0300

    spinner

--- resources/views/livewire/payment-method-create.blade.php
+++ resources/views/livewire/payment-method-create.blade.php
@@ -1,5 +1,10 @@
 <div>
-    <article class="card">
+    <article class="card relative">
+
+        <div wire:loading.flex class="absolute w-full h-full bg-gray-100 bg-opacity-25 z-30 items-center justify-center">
+            <x-spinner size="20"></x-spinner>
+        </div>
+
         <form action="" id="card-form">
             <div class="card-body">
                 <h1 class="text-gray-700 text-lg font-bold mb-4">Agregar método de pago</h1>
@@ -36,37 +41,50 @@
     @slot('js')
 
     <script>
-        const stripe = Stripe('{{config('stripe.key')}}');
-    
-        const elements = stripe.elements();
-        const cardElement = elements.create('card');
-    
-        cardElement.mount('#card-element');
-
-        //Generar token
-        const cardHolderName = document.getElementById('card-holder-name');
-        const cardButton = document.getElementById('card-button');
-        const cardForm = document.getElementById('card-form');
-        const clientSecret = cardButton.dataset.secret;
-
-        cardForm.addEventListener('submit', async (e) => {
-            e.preventDefault();
-
-            const { setupIntent, error } = await stripe.confirmCardSetup(
-                clientSecret, {
-                    payment_method: {
-                        card: cardElement,
-                        billing_details: { name: cardHolderName.value }
-                    }
-                }
-            );
-
-            if (error) {
-                document.getElementById('cardErrors').textContent = error.message;
-            } else {
-                Livewire.emit('paymentMethodCreate', setupIntent.payment_method);
-            }
+        document.addEventListener('livewire:load', function () {
+            stripe();
         });
+
+        Livewire.on('resetStripe', function () {
+            document.getElementById('card-form').reset();
+            stripe();
+        });
+    </script>
+
+    <script>
+        function stripe() {
+            const stripe = Stripe('{{config('stripe.key')}}');
+    
+            const elements = stripe.elements();
+            const cardElement = elements.create('card');
+
+            cardElement.mount('#card-element');
+
+            //Generar token
+            const cardHolderName = document.getElementById('card-holder-name');
+            const cardButton = document.getElementById('card-button');
+            const cardForm = document.getElementById('card-form');
+            const clientSecret = cardButton.dataset.secret;
+
+            cardForm.addEventListener('submit', async (e) => {
+                e.preventDefault();
+
+                const { setupIntent, error } = await stripe.confirmCardSetup(
+                    clientSecret, {
+                        payment_method: {
+                            card: cardElement,
+                            billing_details: { name: cardHolderName.value }
+                        }
+                    }
+                );
+
+                if (error) {
+                    document.getElementById('cardErrors').textContent = error.message;
+                } else {
+                    Livewire.emit('paymentMethodCreate', setupIntent.payment_method);
+                }
+            });
+        }
     </script>
 
     @endslot
