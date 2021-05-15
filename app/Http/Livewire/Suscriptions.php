6a74b1ac15ef0b043a89e32f8b6912b2e9c582b8
Manuel <giudicimanuel@gmail.com>
2021-05-15 19:48:42 -0300

    componente suscripciones

---
+++ app/Http/Livewire/Suscriptions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Suscriptions extends Component
+{
+    public function render()
+    {
+        return view('livewire.suscriptions');
+    }
+}
