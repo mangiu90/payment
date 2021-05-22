01e953cf7db95b36eaa3957e18bc1b17375b8d6f
Manuel <giudicimanuel@gmail.com>
2021-05-22 13:03:05 -0300

    creo middleware para suscripciones

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\BillingController;
 
 Route::get('/', [ProductController::class, 'index'])->name('home');
 Route::get('articles', [ArticleController::class, 'index'])->name('articles.index');
-Route::get('articles/{article}', [ArticleController::class, 'show'])->name('articles.show');
+Route::get('articles/{article}', [ArticleController::class, 'show'])->middleware('subscription')->name('articles.show');
 Route::get('billing', [BillingController::class, 'index'])->middleware('auth')->name('billing.index');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
